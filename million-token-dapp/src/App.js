73de4062ae48d2084b107dd093732b4b4cfa5dbb
jmc171144 <jeremy__mc@DESKTOP-I5BD7MF.localdomain>
2020-03-27 15:18:45 +0100

    :wrench: update App.js

--- million-token-dapp/src/App.js
+++ million-token-dapp/src/App.js
@@ -7,6 +7,9 @@ import { PIXEL_TOKEN_ABI, PIXEL_TOKEN_ADDRESS } from './config.js';
 import Nav from './Nav';
 import About from './About';
 import School from './School';
+import Homepage from './Homepage';
+import Purchase from './Purchase';
+
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 class App extends Component {
@@ -45,20 +48,31 @@ class App extends Component {
         <div>
           <div className="App">
             <Nav />
-            <Route path="/about" component={About}/>
-            <Route path="/school" component={School}/>
+            <Switch>
+              <Route path="/" exact component={Homepage}/>
+              <Route path="/homepage" exact component={Homepage}/>
+              <Route path="/purchase" exact component={Purchase}/>
+              <Route path="/about" component={About}/>
+              <Route path="/school" component={School}/>
+            </Switch>
           </div>
-          <div className="web3_test">
+          {/* <div className="web3_test">
             <h1>Hello, world!</h1>
             <p>Your account: {this.state.account}</p>
             <p>Number of pixels: {this.state.nbOfPixels}</p>
-          </div>
+          </div> */}
         </div>
       </Router>
     );
   }
 
 }
+
+// const Home = () => (
+//   <div>
+//     <h1>Home page</h1>
+//   </div>
+// );
 
 
 
