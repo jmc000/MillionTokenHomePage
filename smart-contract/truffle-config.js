e33950046eb5c753f774783f5736e70d8f330d3c
jmc171144 <jeremy__mc@DESKTOP-I5BD7MF.localdomain>
2020-03-27 16:35:26 +0100

    :package: ganache network

--- smart-contract/truffle-config.js
+++ smart-contract/truffle-config.js
@@ -45,8 +45,8 @@ module.exports = {
     //
     development: {
      host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
+     network_id: "5777",       // Any network (default: none)
     },
     rinkeby: {
       provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/df9dfa5d47f24af6b5566cc9d97da46c`),
